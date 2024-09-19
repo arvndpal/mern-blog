ea834e563bf7ae82f60fb925a019edc68fc7c27b
Arvind Pal <arvind.pal@sunlife.com>
2024-09-19 22:12:34 +0530

    connected with DB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Mongodb connected.');
+  })
+  .catch((error) => {
+    console.log('error', error);
+  });
 
 const app = express();
 app.listen(5172, () => {
-  console.log('App is listening at 5172!!!');
+  console.log('App is listening at 5172.');
 });
