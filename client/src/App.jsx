2269caad07abd46a1e3313c859223b7fdc1bb6b9
Arvind Pal <arvind.pal@sunlife.com>
2024-09-19 21:22:56 +0530

    created header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import About from './pages/About';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Projects from './pages/Projects';
+import Header from './components/Header';
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/dashboard' element={<Dashboard />} />
